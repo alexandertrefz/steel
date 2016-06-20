d49c1dc95f5de8e031bc45f1fa37ab535dfec183
Alexander Trefz <alexander.trefz@gmail.com>
2016-06-20 18:52:35 +0200

    Update Release script

--- release.sh
+++ release.sh
@@ -3,3 +3,4 @@ node_modules/.bin/rollup src/steel.ts -o dist/steel.amd.js -c -f amd
 node_modules/.bin/rollup src/steel.ts -o dist/steel.cjs.js -c -f cjs
 node_modules/.bin/rollup src/steel.ts -o dist/steel.umd.js -c -f umd
 node_modules/.bin/rollup src/steel.ts -o dist/steel.iife.js -c -f iife
+node_modules/.bin/rollup src/steel.ts -o dist/steel.es6.js -c rollup.es.config.js -f es
